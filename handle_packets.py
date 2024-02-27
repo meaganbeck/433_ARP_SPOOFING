9856beb3d1c192b5cc61fab86fa2b340ccc9865f
Meagan Beckstrand <mbeckstrand101@yahoo.com>
2024-02-26 19:04:36 -0800

    smol edits

--- handle_packets.py
+++ handle_packets.py
@@ -1,25 +1,28 @@
 import subprocess
+import os
+import time
+import socket
+import scapy.all import * #dunno if using yet
+
+#TODO: "ethernet" fix
 
 def block_arp_cache():
-
-#dir == stop arp cache
-#TODO: "ethernet" fix
     #block from responding to ANY arp request
     result = subprocess.run(["sysctl", f"net.ipv4.conf.ethernet.arp_ignore=8"], shell=True, capture_output=True, text=True)
+    #check success
+
     if result.returncode == 0:
-        print("success")
+        print("success in blocking response")
     else:
         print(result.stdout)
 
 def block_gratuitous():
     #drop gratuitous arp requests
     result = subprocess.run(["sysctl",f"net.ipv4.conf.ethernet.arp_accept=0"], shell=True, capture_output=True, text=True)
+        
+        #check success
         if result.returncode == 0:
-            print("success")
+            print("success in dropping gratuitous packet")
         else:
             print(result.stdout)
-
-def notify_user():
-
-#proxy_arp?
 
