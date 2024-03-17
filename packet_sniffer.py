7ca188bb92e5e95ff87e7c60f17aa104ad0daee5
hfigone <hfigone@walnutvirtual.walnutvirtual.virtualbox.org>
2024-03-17 12:13:19 -0700

    Added print statements for testing.

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -52,7 +52,7 @@ try:
 
     # sniff for packets continuously
     for packet in capture.sniff_continuously():
-        print("Sniffing...") # DEBUG 
+       # print("Sniffing...") 
 
         # Every few seconds, purge the request queue of expired requests 
         if (time.time() - alarm >= 4):
@@ -64,7 +64,7 @@ try:
         packet_deets = Packet_Info(arp_layer.src_hw_mac, arp_layer.dst_hw_mac, arp_layer.src_proto_ipv4, arp_layer.dst_proto_ipv4, int(arp_layer.opcode, 16))
         
         if (packet_deets.src_ip == my_IP and packet_deets.opcode == 1): #outgoing request
-            # print(f"Outgoing request to address {packet_deets.dest_ip}")
+            print(f"Outgoing request to address {packet_deets.dest_ip}")
             # add to request queue
             packet_deets.expiry = time.time() + 2
             enqueue(request_queue, packet)
@@ -72,25 +72,28 @@ try:
         elif packet_deets.dest_ip == my_IP: #incoming
 
             if packet_deets.opcode == 1: # request 
-                # print(f"Incoming request from address {packet_deets.src_ip}")
+                print(f"Incoming request from address {packet_deets.src_ip}")
                 # check the arp cache. if a mapping exists whose entry does not match the one in the request, the person who sent it is spoofing
                 if (packet_deets.src_ip in arp_cache):
                     if (arp_cache[packet_deets.src_ip].src_mac != packet_deets.src_mac):
+                        print("Spoofed request detected. Purging...")
                         del arp_cache[packet_deets.src_ip] # purge the entry from the arp cache 
                         subprocess.run(["arp", "-d", f"{packet_deets.src_ip}"])
 
             elif packet_deets.opcode == 2: # reply
-                # print(f"Incoming reply from address {packet_deets.src_ip}") 
+                print(f"Incoming reply from address {packet_deets.src_ip}") 
                 # Check request queue. if a corresponding request is present (reply was solicited):
                 if (packet_deets.src_ip in arp_cache):
                     # if the request is marked complete (i.e. we received two replies to it): 
                     if arp_cache[packet_deets.src_ip].complete: 
+                        print("Spoofed reply detected. Purging...")
                         del arp_cache[packet_deets.src_ip] # purge the entry from the arp cache
                         subprocess.run(["arp", "-d", f"{packet_deets.src_ip}"]) # purge the request from the request queue 
                         # optional: generate a new arp request and send it to the ip in question
                 # if it is incomplete AND not expired: generate a reply as normal 
                     # the arp module will do this as normal and we don't need to do anything 
                 else: # otherwise it's unsolicited and we don't want it in the arp cache 
+                    print("Unsolicited reply detected. Purging...")
                     subprocess.run(["arp", "-d", f"{packet_deets.src_ip}"]) 
                  
 except KeyboardInterrupt:
