ca44b8f630a5619d9fe45929dd4c2a2bc18cbeb6
Leo Abrahamian <leo.abrahamian@outlook.com>
2026-03-14 16:59:39 -0700

    Code commenting on main.py, plan on changing subprocesses.Popen to subprocesses.run.

--- main.py
+++ main.py
@@ -6,27 +6,27 @@ import time
 import socket
 
 try:
-	import netifaces
-	import subprocess
+    import netifaces
+    import subprocess
 except ModuleNotFoundError:
-	print("Please ensure that you have installed the netifaces module for python (pip3 install netifaces).") 
-	exit()
+    print("Please ensure that you have installed the netifaces module for python (pip3 install netifaces).") # except net-tools is deprecated 
+    exit()
 
 def main(): 
 	# Scan for interface names 
-	network_interfaces = netifaces.interfaces()
+    network_interfaces = netifaces.interfaces()
 
-	for i in range(len(network_interfaces)): 
-		interface_name = network_interfaces[i]
-		if not (interface_name.startswith('lo')): # skip loopback interfaces
-			addresses = netifaces.ifaddresses(interface_name)
-			interface_IP = addresses[netifaces.AF_INET][0]['addr']
-			interface_MAC = addresses[netifaces.AF_LINK][0]['addr']
-			print("Interface: " + interface_name)
-			print("MAC: " + interface_MAC)
-			print("IP: " + interface_IP) 
-			subprocess.Popen(["./packet_sniffer.py", interface_name, interface_IP, interface_MAC])
-	return 0
+    for i in range(len(network_interfaces)): 
+        interface_name = network_interfaces[i]
+        if not (interface_name.startswith('lo')): pass # skip loopback interfaces
+        addresses = netifaces.ifaddresses(interface_name) # store the addresses associated with the interface
+        interface_IP = addresses[netifaces.AF_INET][0]['addr'] 
+        interface_MAC = addresses[netifaces.AF_LINK][0]['addr'] # and the corr address associated at link layer 
+        print("Interface: " + interface_name)
+        print("MAC: " + interface_MAC)
+        print("IP: " + interface_IP) 
+        subprocess.Popen(["./packet_sniffer.py", interface_name, interface_IP, interface_MAC]) # and now invoke packet_sniffer using the stored arguments
+    return 0
 
 if __name__ == "__main__":
-	main()
+    main()
